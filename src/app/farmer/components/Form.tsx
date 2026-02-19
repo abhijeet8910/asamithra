062f833dd3f2395ea22dd85a0600914d51bdc539
abhijeet8910 <abhidars456@gmail.com>
2026-02-19 14:28:51 +0530

    Farmer login Added

---
+++ src/app/farmer/components/Form.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/components/login-form'
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className=''>
+      <LoginForm className=''/>
+    </div>
+  )
+}
+
+export default Form
--- src/app/farmer/page.tsx
+++ src/app/farmer/page.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Form from './components/Form'
 
 const page = () => {
   return (
-    <div>
-      farmer page
-    </div>
+    <section className='relative min-h-screen flex items-center '>
+      {/* Background */}
+       
+      {/* Fixed Background Image */}
+      <div
+        className="fixed inset-0 bg-cover bg-center bg-no-repeat -z-10"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1620200423727-8127f75d7f53?w=1600')",
+        }}
+      />
+  
+      {/* Dark Overlay */}
+      <div className="fixed inset-0 bg-black/60 -z-10" />
+        {/* content */}
+      <main className='w-full max-w-md mx-auto p-6'>
+         <Form/> 
+      </main>
+    </section>
   )
 }
 
